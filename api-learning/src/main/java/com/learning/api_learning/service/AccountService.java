9bfb25f3d790fdf963175cd634387a59aba38c49
Rahul Balashanmugam <tsca.rahul@outlook.com>
2025-05-19 22:06:08 +0100

    Commit

---
+++ api-learning/src/main/java/com/learning/api_learning/service/AccountService.java
@@ -0,0 +1,43 @@
+package com.learning.api_learning.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.learning.api_learning.model.Account;
+import com.learning.api_learning.repository.AccountRepository;
+
+
+@Service
+public class AccountService {
+    
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account){
+        account.setBalance(account.getBalance() == null ? 0.0 : account.getBalance());
+        return accountRepository.save(account);
+    }
+
+    public Account deposit (Long id, Double amount){
+        Account account = getAccount(id);
+        Double newBalance = account.getBalance() + amount;
+        accountRepository.updateBalance(id, newBalance);
+        account.setBalance(newBalance);
+        return account;
+    }
+
+    public Account withdraw(Long id, Double amount){
+        Account account = getAccount(id);
+        if (account.getBalance() < amount){
+            throw new RuntimeException("Insufficient funds");
+        }
+        Double newBalance = account.getBalance() - amount;
+        accountRepository.updateBalance(id, newBalance);
+        account.setBalance(newBalance);
+        return account;
+    }
+
+    public Account getAccount(Long id){
+        return accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account not found"));
+    }
+}
---
+++ api-learning/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE accounts (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    account_holder VARCHAR(255),
+    balance DECIMAL(15, 2)
+);
